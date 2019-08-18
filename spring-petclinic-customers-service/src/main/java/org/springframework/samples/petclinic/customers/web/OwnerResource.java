d1a733b867d03853bdda7f7c4ff5127beb9c1742
rakeshpriyad <rakeshpriyad@gmail.com>
2019-08-18 11:25:47 +0530

    first commit

---
+++ spring-petclinic-customers-service/src/main/java/org/springframework/samples/petclinic/customers/web/OwnerResource.java
@@ -0,0 +1,104 @@
+/*
+ * Copyright 2002-2017 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.springframework.samples.petclinic.customers.web;
+
+import java.util.List;
+import java.util.Optional;
+
+import javax.validation.Valid;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.samples.petclinic.customers.model.Owner;
+import org.springframework.samples.petclinic.customers.model.OwnerRepository;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import io.micrometer.core.annotation.Timed;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * @author Juergen Hoeller
+ * @author Ken Krebs
+ * @author Arjen Poutsma
+ * @author Michael Isvy
+ * @author Maciej Szarlinski
+ */
+@RequestMapping("/owners")
+@RestController
+@Timed("petclinic.owner")
+@RequiredArgsConstructor
+@Slf4j
+class OwnerResource {
+
+	@Autowired
+    private OwnerRepository ownerRepository;
+	
+	Logger log = LoggerFactory.getLogger(OwnerResource.class);
+
+    /**
+     * Create Owner
+     */
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Owner createOwner(@Valid @RequestBody Owner owner) {
+        return ownerRepository.save(owner);
+    }
+
+    /**
+     * Read single Owner
+     */
+    @GetMapping(value = "/{ownerId}")
+    public Optional<Owner> findOwner(@PathVariable("ownerId") int ownerId) {
+        return ownerRepository.findById(ownerId);
+    }
+
+    /**
+     * Read List of Owners
+     */
+    @GetMapping
+    public List<Owner> findAll() {
+        return ownerRepository.findAll();
+    }
+
+    /**
+     * Update Owner
+     */
+    @PutMapping(value = "/{ownerId}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void updateOwner(@PathVariable("ownerId") int ownerId, @Valid @RequestBody Owner ownerRequest) {
+        final Optional<Owner> owner = ownerRepository.findById(ownerId);
+
+        final Owner ownerModel = owner.orElseThrow(() -> new ResourceNotFoundException("Owner "+ownerId+" not found"));
+        // This is done by hand for simplicity purpose. In a real life use-case we should consider using MapStruct.
+        ownerModel.setFirstName(ownerRequest.getFirstName());
+        ownerModel.setLastName(ownerRequest.getLastName());
+        ownerModel.setCity(ownerRequest.getCity());
+        ownerModel.setAddress(ownerRequest.getAddress());
+        ownerModel.setTelephone(ownerRequest.getTelephone());
+        log.info("Saving owner {}", ownerModel);
+        ownerRepository.save(ownerModel);
+    }
+}
